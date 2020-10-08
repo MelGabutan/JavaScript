ddade3d2008e2436cc6d2400898d55d98fe57a9d
Mel Jefferson T. Gabutan <70356954+MelGabutan@users.noreply.github.com>
2020-10-08 13:27:19 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+function printHome(){
+    document.getElementById("Name").innerHTML = "Welcome to my <br> very special page"; 
+}
+
+function printTextAndImage(){
+    document.getElementById("chitoge").src = "chitoge.png"; 
+    document.getElementById("text-one").innerHTML = "In sed quam diam. Praesent a mi at massa varius convallis. Quisque varius ligula condimentum, porttitor nunc sit amet, laoreet tellus. Donec facilisis nibh a orci ultricies, vel faucibus lectus consequat. Nam vestibulum faucibus maximus. Sed lobortis pellentesque tortor et consequat. Curabitur dignissim ipsum vel ex facilisis convallis. Curabitur nec imperdiet velit."; 
+    document.getElementById("text-two").innerHTML = "Vivamus porttitor massa ac tellus pellentesque, sed dignissim quam luctus. Praesent faucibus eleifend risus ut congue. Suspendisse ut lacus et orci finibus cursus sit amet sit amet nulla. Sed non tellus ac metus pretium consequat ut nec massa. Nullam non dictum mauris, nec finibus nunc. In elementum dui eu consequat bibendum. Aliquam dapibus, nisl in cursus fringilla, nisi nisl porta felis, quis condimentum ante odio vel tortor. Morbi ut ultricies mi, convallis placerat ante. Curabitur nec justo laoreet, posuere diam eu, commodo sapien. In accumsan justo quis orci convallis, vel accumsan nisi porta. Integer diam est, venenatis at eleifend vel, interdum in quam. Nullam et neque lacinia, porttitor nulla quis, dignissim nulla. Etiam pellentesque nisi sed sem consectetur cursus. Quisque ex sapien, posuere sed accumsan a, semper sed nunc. Ut tincidunt lectus quis tempor sodales."; 
+    document.getElementById("text-three").innerHTML = "Aenean vehicula vitae nisi id condimentum. Aliquam erat volutpat. Donec laoreet sollicitudin enim at iaculis. Aliquam placerat est turpis, a dictum nibh pretium eget. Duis vehicula lectus et lacus commodo, eget laoreet nunc suscipit. Fusce rutrum nec nisi luctus egestas. Sed quis orci nibh. Curabitur vel semper diam. Integer efficitur, quam ac posuere imperdiet, erat quam pharetra magna, et efficitur lacus odio quis lectus. Nullam consequat, justo eget varius blandit, mi quam ullamcorper orci, non mattis orci metus et eros."; 
+    document.getElementById("text-four").innerHTML = "In sed quam diam. Praesent a mi at massa varius convallis. Quisque varius ligula condimentum, porttitor nunc sit amet, laoreet tellus. Donec facilisis nibh a orci ultricies, vel faucibus lectus consequat. Nam vestibulum faucibus maximus. Sed lobortis pellentesque tortor et consequat. Curabitur dignissim ipsum vel ex facilisis convallis. Curabitur nec imperdiet velit."; 
+    document.getElementById("text-five").innerHTML = "Pellentesque a lectus velit. Mauris vitae dui nulla. In vel dapibus odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut vel iaculis libero. Sed sit amet mauris est. Pellentesque molestie metus eros, et dictum dolor suscipit vel. Sed vulputate condimentum tempus. Cras orci neque, semper quis ultrices maximus, maximus convallis augue. Morbi vehicula elementum dolor ac vestibulum. Vestibulum vel elit faucibus, aliquam justo vitae, molestie tortor. Maecenas eget tellus lectus."; 
+}
+
+function computeAge(){
+    var name = prompt("Enter your name:")
+    var year = prompt("Enter your birth year:")
+    document.getElementById("user-name").innerHTML = name;
+    document.getElementById("user-birth-year").innerHTML = year;
+    var age = 2020-year; 
+    document.getElementById("user-age").innerHTML = age;
+}
+
+function changeParagraphColor(){
+    var backgroundColor = prompt("Enter paragraph's background color");
+    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor tempus lectus in maximus. Aenean ut orci vestibulum, volutpat quam eu, tincidunt nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.";
+    document.getElementById("paragraph").innerHTML = text + "Pellentesque a lectus velit. Mauris vitae dui nulla. In vel dapibus odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut vel iaculis libero. Sed sit amet mauris est. Pellentesque molestie metus eros, et dictum dolor suscipit vel. Sed vulputate condimentum tempus. Cras orci neque, semper quis ultrices maximus, maximus convallis augue. Morbi vehicula elementum dolor ac vestibulum. Vestibulum vel elit faucibus, aliquam justo vitae, molestie tortor. Maecenas eget tellus lectus.";
+    document.getElementById("paragraph").style.background = backgroundColor;
+}
+
+function talkWithMel(){
+    var answerPositive;
+    var answerNegative;
+    var flag;
+    var userName;
+    var isAnimeFan;
+    var favoriteAnime;
+    var bestWaifu;
+    var answer = prompt("Hello! How are you feeling today?");
+
+    if(answer == "I'm feeling good") 
+    {
+        flag = confirm("That's great! We should be friends!");
+    }
+    else if(answer == "I'm feeling bad")
+    {
+        flag = confirm("Cheer up! I'm here for you. Let's be friends");
+    }
+
+    if(flag == true)
+    {
+        userName = prompt("We should introduce ourselves. I'm Mel, what is your name?");
+        alert("Hi " + userName + "! it's nice to meet you!");
+        isAnimeFan = prompt("Do you watch animes? (Yes or No)");
+        if(isAnimeFan == "Yes")
+        {
+            favoriteAnime = prompt("What is your favorite anime?");
+            if(favoriteAnime == "Nisekoi")
+            {
+                alert("I declare on this very day that we leveled up into buddies");
+                bestWaifu = prompt("Best Waifu: Chitoge (duhh obviously) or Onodera");
+                if(bestWaifu == "Chitoge")
+                {
+                    alert("Relationship level unlocked: Mel's Bestfriend. Good bye bestfriend!");
+                }
+                else if(bestWaifu == "Onodera")
+                {
+                    alert("Good bye stranger!");
+                }
+                else
+                {
+                    alert("Input properly next time my friend! Good bye");
+                }
+            }
+            else
+            {
+                alert("If that's the case, you never saw Nisekoi. Go watch it now!");
+            }
+        }
+        else if(isAnimeFan == "No")
+        {
+            alert("Nani. I suggest that you watch Nisekoi! It's a great show. Go watch it now. Good bye!");
+        }
+        else
+        {
+            alert("Input properly next time my friend! Good bye");
+        }
+    }
+    else
+    {
+        alert("You made Mel sad :( Good bye");
+    }
+
+}
